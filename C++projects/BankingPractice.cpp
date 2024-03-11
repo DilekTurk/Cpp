eca5343f6553df0241f4b1910bfa6df65ae022a1
Dilek Türk <turkdilek911@gmail.com>
2024-03-11 23:31:58 +0300

    Add files via upload

---
+++ C++projects/BankingPractice.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <limits>
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main(){
+
+    int choice=0;
+    int balance=0;
+
+    do{
+
+        std::cout<<"Please enter your choice: \n";
+        std::cout<<"Show balance: 1\n"<<"Deposit: 2\n"<<"Withdraw: 3\n"<<"Exit: 4\n";
+        std::cin>>choice;
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch (choice){
+        case 1: showBalance(balance);
+                break;
+        case 2: balance+=deposit();
+                break;
+        case 3: balance-=withdraw(balance);
+                break;
+        case 4: std::cout<<"Thanks for visiting!!!";  
+                break;  
+        default: std::cout<<"Please enter a valid choice: ";   
+        }
+
+    }while(choice!=4);
+    
+    return 0;
+}
+void showBalance(double balance){
+    std::cout<<"Your balance: $"<<balance<<'\n';
+}
+
+double deposit(){
+    double amount;
+    std::cout<<"Amount: ";
+    std::cin>>amount;
+    if(amount>0){
+        return amount;
+    }
+    else{
+        std::cout<<"Please enter a valid amount.\n";
+        return 0;
+    }
+}
+    
+double withdraw(double balance){
+    double amount;
+    std::cout<<"Amount: ";
+    std::cin>>amount;
+    if(amount>balance){
+        std::cout<<"Please enter a valid amount.\n";
+        return 0;
+    }
+    else if(amount<0){
+        std::cout<<"Please enter a valid amount.\n";
+        return 0;
+    }
+    else{
+        return amount;
+    }
+}
---
+++ C++projects/RandomEvent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+int main(){
+
+    srand(time(0));
+    int randNum = (rand()%5)+1;
+    switch(randNum){
+        case 1: std::cout<<"Study.";
+        break;
+        case 2: std::cout<<"Play a video game.";
+        break;
+        case 3: std::cout<<"Watch a movie.";
+        break;
+        case 4: std::cout<<"Tidy your bedroom.";
+        break;
+        case 5: std::cout<<"Read a book.";
+        break;
+    }
+}
